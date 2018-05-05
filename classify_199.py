ef734e910676302db610b645952be9f435b371a5
e-stranger <jchaunceya@g.ucla.edu>
2018-05-04 20:55:48 -0700

    save classify.py before removal

--- classify_199.py
+++ classify_199.py
@@ -1,26 +1,40 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import MinMaxScaler
+import logging
+import pandas as pd
 
-from sklearn.ensemble import ExtraTreesClassifier
-
-import pandas
+logger = logging.getLogger(__name__)
 
 def get_grouped_labels(label):
-	"""Assumes labels `vneg`, `neg`, `neu`, `pos`, `vpos`"""
+	"""Assumes labels `vneg`, `neg`, `neu`, `pos`, `vpos`."""
 	if label == 'vneg':
 		return 'neg'
 	elif label == 'vpos':
 		return 'pos'
 	return label
 
-if __name__ == "__main__":
-	df = pandas.read_csv("data/articles_sample.csv")
+def load_df(filename="data/articles_sample.csv", **kwargs):
+	"""
+	Load dataset. Defaults to the sample.
+	If it fails to load, exits with status 1. 
+	"""
+	try:
+		if kwargs:
+			for name, value in kwargs.items():
+				print("{}: {}".format(name,value))
+		df = pd.read_csv(filename, **kwargs)
+	except FileNotFoundError:
+		logger.exception("File {} not found.".format(filename))
+		exit(1)
+
+	return df
+
+		
+def main():
+	df = pd.read_csv("data/articles_sample.csv")
 	classified = df.loc[df['new?'].apply(lambda x: x == 1)]
 	classified = classified.loc[classified['class_t'].apply(lambda x: x != "dm")]
 	classified.to_csv("classfied.csv")
@@ -34,3 +48,5 @@ if __name__ == "__main__":
 	print(cross_val_score(clf, doc_vectors, grouped_labels))
 	cvp = cross_val_predict(clf, doc_vectors, grouped_labels)
 	print(confusion_matrix(grouped_labels, cvp , labels=["pos", "neu", "neg"]))
+if __name__ == "__main__":
+	load_df("fils", big_jim = "hello, world!")
