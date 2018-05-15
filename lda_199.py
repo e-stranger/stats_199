168fd17fb90a41efa85138b1dbbbca2f42b9036c
e-stranger <jchaunceya@g.ucla.edu>
2018-05-15 01:21:49 -0700

    modify gitignore, clean repo

--- lda_199.py
+++ lda_199.py
@@ -1,45 +1,51 @@
-from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.decomposition import LatentDirichletAllocation, NMF, PCA
+from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-import pandas, sys
-
+from sklearn.preprocessing import MinMaxScaler
+from utils import get_logger, get_now
+import pandas, sys, json, datetime, re, pickle
+import numpy as np
 # Latent Dirichlet Allocation with Scikit-Learn
 
 sample_filename = 'data/articles_sample.csv'
 
+logger = get_logger(__name__)
 # from
 # http://scikit-learn.org/stable/auto_examples/applications/plot_topics_extraction_with_nmf_lda.html
 
-s = """WASHINGTON  —     Donald J. Trump on Tuesday named as his chief trade negotiator a Washington lawyer who has long advocated protectionist policies, the latest sign that Mr. Trump intends to fulfill his campaign promise to get tough with China, Mexico and other trading partners. Mr. Trump also renewed his episodic campaign to persuade American companies to expand domestic manufacturing, criticizing General Motors via Twitter on Tuesday morning for making in Mexico some of the Chevrolet Cruze hatchbacks it sells domestically. Hours later, Mr. Trump claimed credit after Ford said it would expand vehicle production in Flat Rock, Mich. The choice of Robert Lighthizer (pronounced   ) to be the United States’ trade representative nearly completes Mr. Trump’s selection of top economic advisers and, taken together with the  ’s running commentary on Twitter, underscores Mr. Trump’s focus on making things in America. That is causing unease among some Republicans who regard Mr. Trump’s views on trade as dangerously retrograde, even as they embrace the bulk of his economic agenda.  Mainstream economists warn that protectionist policies like import taxes could impose higher prices on consumers and slow economic growth. But some Democrats are signaling a readiness to support Mr. Trump. Nine House Democrats held a news conference Tuesday with the A. F. L. . I. O. president, Richard Trumka, to urge renegotiation of the North American Free Trade Agreement with Mexico and Canada. “We wanted him to know that we’ll work with him on doing that,” Mr. Trumka said. “I don’t think he has enough Republican support to do it, and rewriting the rules of trade is a necessary first step in righting the economy for working people. ” Mr. Trump and his top advisers on trade, including Mr. Lighthizer, share a view that the United States in recent decades prioritized the ideal of free trade over its own  . They argue that other countries are undermining America’s industrial base by subsidizing their own export industries while impeding American importers. They regard this unfair competition as a key reason for the lackluster growth of the economy. In picking Mr. Lighthizer, who has spent much of the last few decades representing American steel producers in their frequent litigation of trade disputes, Mr. Trump is seeking to hire one of Washington’s top trade lawyers to enforce international trade agreements more vigorously. He must be confirmed by the Senate. “He will do an amazing job helping turn around the failed trade policies which have robbed so many Americans of prosperity,” Mr. Trump said in a statement. Mainstream Republicans have sought common ground with Mr. Trump, emphasizing, for example, the importance of enforcing trade rules, but they have not abandoned the party’s longtime advocacy for trade. Senator Orrin Hatch of Utah, the chairman of the Senate Finance Committee, which will hold hearings on Mr. Lighthizer’s nomination, issued a cautiously supportive statement Tuesday. “As the world and our economic competitors move to expand their global footprints, we can’t afford to be left behind in securing strong deals that will increase our access to new markets for   products and services,” Mr. Hatch said in a statement. “I look forward to a vigorous discussion of Bob’s trade philosophy and priorities. ” Mr. Trump has named a number of advisers on trade, leaving some ambiguity about the division of responsibilities. The   named the economist Peter Navarro, an outspoken critic of China, to lead a new White House office overseeing trade and industrial policy. Mr. Trump also said Wilbur Ross, the billionaire investor and choice for commerce secretary, will play a key role. Mr. Lighthizer, however, is the only member of the triumvirate with government experience. “Those who say U. S. T. R. will be subordinated to other agencies are mistaken,” said Alan Wolff, another former senior American trade official who was the steel industry’s   on trade with Mr. Lighthizer for nearly 20 years, citing Mr. Lighthizer’s encyclopedic knowledge of trade law. “He’ll be a dominant figure on trade, in harmony with Wilbur Ross and Navarro. ” There is also an ideological divide between the people Mr. Trump has named to oversee trade policy and his broader circle of advisers, which is populated by longstanding trade advocates like Gary D. Cohn, the president of Goldman Sachs, who will lead the National Economic Council Rex W. Tillerson, the chief executive of Exxon Mobil, tapped for secretary of state and Gov. Terry Branstad of Iowa, Mr. Trump’s choice for ambassador to China. Proponents of trade hope the broader circle, and congressional Republicans, will exert a moderating influence. “You’re seeing a pretty clear indication that there will be a focus on the enforcement of our trade agreements and on the letter of the law,” said Scott Lincicome, an international trade lawyer at White  Case. “But that doesn’t necessarily mean a significant turn toward protectionism. Even free trade guys like me support enforcement. ” Trade opponents on the left and the right, meanwhile, are hoping Mr. Trump means to break with several decades of   policy. “There’s going to be a war within the Trump administration on where they go with trade, and we’re hoping to energize the worker base he had to make sure they go in the right direction to benefit the American worker,” Mr. Trumka said. Mr. Trump’s promise to immediately designate China as a currency manipulator may offer an early test of the administration’s intentions. Economists see no evidence China is suppressing the value of its currency, although it has done so in the past. Mr. Lincicome said officially labeling China a currency manipulator despite the lack of recent evidence would signal that the administration is taking a hard line on trade issues. A broader shift in trade policy would unfold more slowly. Mr. Trump has promised to renegotiate Nafta the original process took most of three years. He has promised to pursue enforcement actions against other nations, but it takes time to mount cases. He has threatened to impose new tariffs on imports, but sweeping changes most likely would require congressional legislation. Mr. Trump already is seeking to exert influence by seizing the presidential bullhorn. “General Motors is sending Mexican made model of Chevy Cruze to U. S. car   free across border,” he wrote Tuesday on Twitter. “Make in U. S. A. or pay big border tax!” General Motors announced in 2015 that it would make the Cruze in Coahuila, Mexico. American manufacturers are moving   production to Mexico to take advantage of lower labor costs and because of declining domestic demand. They continue to build more expensive vehicles in the United States. Ford’s announcement Tuesday does not reverse that trend. The carmaker said it still planned to move production of the compact Ford Focus from Michigan to Mexico. But it said it would invest in a different Michigan plant to expand production of   vehicles, including its   pickup truck and the Mustang sports car, as well as a new   sport utility vehicle. “We are encouraged by the   policies that   Trump and the new Congress have indicated they will pursue,” said the company’s chief executive, Mark Fields. Mr. Lighthizer served as deputy United States trade representative in the Ronald Reagan administration, when he was involved in pressing Japan to reduce its restrictions on American imports and its subsidies for its own exports. Mr. Trump has criticized China for similar practices, setting the stage for a new round of confrontations. Reagan is often remembered as an advocate for free trade, but his administration in its early hours imposed a quota on Japanese auto imports. It was the first in a long series of measures aimed at putting pressure on the nation that was then regarded, like China in recent years, as a threat to American prosperity. “President Reagan’s pragmatism contrasted strongly with the utopian dreams of free traders,” Mr. Lighthizer wrote in a 2008 piece criticizing Senator John McCain, Republican of Arizona, for embracing “unbridled” free trade. Conservatives, he argued, “always understood that trade policy was merely a tool for building a strong and independent country with a prosperous middle class. """
-def eval_lda(filename=sample_filename, n_topics=10):
+def write_words_to_file(object, method, filename=None):
+	object['method'] = method
+	object['time'] = get_now()
 
-	if len(sys.argv) == 2:
-		filename = sys.argv[1]
+	if not filename:
+		filename = 'word_data/iter{}.json'.format(logger.get_iter())
+	with open(filename, 'w') as file:
+		file.write(json.dumps(object, indent=4))
 
-	if len(sys.argv) == 3:
+def eval_lda(model, filename, n_topics, n_words, ngram_range, **kwargs):
+	vec, lda_object = run_topic_modelling(model, filename=filename, n_topics=n_topics, ngram_range=ngram_range)
+	results = return_top_words(lda_object, vec.get_feature_names(), n_words)
+	write_words_to_file(results, model)
+
+def return_top_words(model, feature_names, n_top_words):
+	""" Saves top n_top_words from each topic, saves to json"""
+	topic_object = {}
+	topic_object['n_topics'] = len(model.components_)
+	for topic_idx, topic in enumerate(model.components_):
 		try:
-			filename = sys.argv[1]
-			n_topics = int(sys.argv[2])
-			print(n_topics)
-		except:
-			print("invalid integer")
-			exit(1)
+			top_words = topic.argsort()[:-n_top_words - 1:-1]
+		except IndexError:
+			logger.exception("Asked for more words than existed in topic vocabulary")
+			top_words = topic.argsort()
+		topic_object[topic_idx] = [feature_names[i] for i in top_words.tolist()]
+	return topic_object
 
-	vec, lda_object = lda(filename=filename, n_topics=n_topics)
-	print_top_words(lda_object, vec.get_feature_names(), 40)
-	print(lda_object.transform(vec.transform([s])))
-
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        message = "Topic #%d: " % topic_idx
-        message += " | ".join([feature_names[i]
-                             for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print(message)
-    print()
-
-
-def load_dataset(filename):
+def load_dataset(filename, sample=None):
 	try:
-		return pandas.read_csv(filename)
+		df = pandas.read_csv(filename)
+		if sample:
+			df = df.sample(sample)
+		return df
 	except FileNotFoundError:
 		print("File not found!")
 		return None
@@ -47,16 +53,105 @@ def load_dataset(filename):
 		print(str(e))
 		return None
 
-# assumes content is in column named # `content`
-def lda(filename, n_topics=10):
+def run_kmeans(filename, n_topics, ngram_range):
 	data = load_dataset(filename)
-	data.dropna(subset=['content'], inplace=True)
-	vec = TfidfVectorizer(max_features=1000, stop_words='english', ngram_range=(2,2))
-	transformed_docs = vec.fit_transform(data['content'])
-	lda = LatentDirichletAllocation(n_components=n_topics, max_iter=100)
-	lda.fit(transformed_docs)
-	return vec, lda
+	data.dropna(subset=['content', 'title'], inplace=True)
+	vec = TfidfVectorizer(stop_words='english', ngram_range=ngram_range, max_features=1500)
+	titles = data['title'].apply(lambda x: re.sub("( - Breitbart)|([\s]+$)|(^[\s]+)|(as it happened)|(\u2019)|(\u2018)|(\u2013)|(\u201c)|(\u201d)|(\u00a0)|( - The New York Times)", " ", x))
+	titles = titles.apply(lambda x: re.sub("[\s]+", u" ", x))
+	transformed_docs = vec.fit_transform(titles)
+	topic_model = KMeans(n_clusters=n_topics)
+	clusters = topic_model.fit_predict(transformed_docs)
+	clusters_labels = [{'index': index.item(), 'cluster_label': cluster_label.item(), 'title': title , 'publication': publication} for index,cluster_label,title, publication in zip(np.arange(0, clusters.shape[0]), clusters, titles, data['publication'])]
+	cluster_df = pandas.DataFrame(clusters_labels)
+	cluster_df.to_csv("clusters.csv")
+	groups = {}
+	for i in range(0, n_topics):
+		groups[str(i)] = []
+	for cl in clusters_labels:
+		groups[str(cl['cluster_label'])].append((cl['index'], cl['title']))
+	write_words_to_file(groups, 'kmeans')
+
+
+# assumes content is in column named # `content`
+def run_topic_modelling(model, filename, n_topics, ngram_range):
+	data = load_dataset(filename)
+	data.dropna(subset=['content', 'title'], inplace=True)
+	vec = TfidfVectorizer(stop_words='english', ngram_range=ngram_range, max_features=1000)
+	transformed_docs = vec.fit_transform(data['title'])
+	if model == "LDA":
+		topic_model = LatentDirichletAllocation(n_components=n_topics, max_iter=100)
+	else:
+		topic_model = NMF(n_components=n_topics, max_iter=1000, random_state=1, solver="mu", beta_loss='kullback-leibler')
+	topic_model.fit(transformed_docs)
+
+	# Save pickled vectorizer, LDA object for later!
+	# We will need this to assign labels later on.
+
+	with open("pickle/lda_pickled.pkl", "wb") as file:
+		pickle.dump(topic_model, file)
+	with open("pickle/vec_pickled.pkl", "wb") as file:
+		pickle.dump(vec, file)
+	return vec, topic_model
+
+def main(model, filename, n_topics, n_words, ngram_range):
+	if model == "kmeans":
+		run_kmeans(filename, n_topics, ngram_range)
+	else:
+		eval_lda(model, filename, n_topics, n_words, ngram_range)
+
+# Want to get everything since inauguration: Jan 20th, 2017
+
+def filter_data():
+	filenames = ["article_data/articles1.csv", "article_data/articles2.csv", "article_data/articles3.csv"]
+	df = pandas.DataFrame()
+	for filename in filenames:
+		df = df.append(pandas.read_csv(filename))
+
+
+	inauguration = datetime.datetime(year=2017, day=20, month=1)
+	df.date = pandas.to_datetime(df.date, yearfirst=True)
+	df = df.loc[df.date > inauguration]
+	df.to_csv("since_inaug.csv")
+
+def convert_to_n_dim(filename, n_topics):
+	try:
+		df = pandas.read_csv(filename)
+	except FileNotFoundError:
+		logger.exception("File not found while attempting PCA!")
+		exit(1)
+	publications = pandas.unique(df['publication'])
+	init_matrix = np.zeros((publications.shape[0], n_topics))
+
+	index_converter = {}
+	for i,publication in enumerate(publications):
+		index_converter[publication] = i
+
+	for i,j in enumerate(df.index):
+		cluster = df.loc[j, 'cluster_label']
+		init_matrix[index_converter[df.loc[j, 'publication']], cluster] += 1
+
+	pca_model = PCA(n_components=2)
+
+	return pca_model, init_matrix
+
 
 
 if __name__ == "__main__":
-	eval_lda(filename='breitbart_articles.csv')
+	try:
+		filename = 'article_data/{}'.format(sys.argv[1])
+		model = sys.argv[2]
+		if model != "LDA" and model != "NMF" and model != "kmeans":
+			logger.error("Model must be either LDA or NMF! Exiting...")
+			exit(1)
+		n_topics = int(sys.argv[3])
+		n_words = int(sys.argv[4])
+		ngram_lower = int(sys.argv[5])
+		ngram_upper = int(sys.argv[6])
+		ngram_range = (ngram_lower, ngram_upper)
+		main(model, filename, n_topics, n_words, ngram_range=ngram_range )
+
+	except IndexError:
+		print("Supply filename argument!")
+		exit(1)
+
